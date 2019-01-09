d1a3552cdf5ba6c12b1f035af4fcd86e94a15056
Philipp Kriegbaum <philippkriegbaum@Philipps-MacBook-Pro.local>
2019-01-09 16:00:00 +0100

    Finished RankBasedRouletteWheelSelection

--- src/main/selection/RankBasedRouletteWheelSelection.java
+++ src/main/selection/RankBasedRouletteWheelSelection.java
@@ -11,26 +11,38 @@ import java.util.Collections;
 public class RankBasedRouletteWheelSelection extends Selection {
     public ArrayList<Tour> doSelection(Population population) {
         ArrayList<Tour> populationTourList = population.getTourList();
-        ArrayList<Tour> returnList = new ArrayList<Tour>();
+        ArrayList<Tour> returnPopulationTourList = new ArrayList<Tour>();
         Collections.sort(populationTourList);
+
+        // Generates the number of tours for the roulette wheel (maximum 10%)
+        int maximumNumberOfIndividualsSelected = (int) Configuration.instance.randomGenerator.nextDouble(0, 0.1) * populationTourList.size();
+        if (maximumNumberOfIndividualsSelected % 2 != 0)
+            maximumNumberOfIndividualsSelected += 1;
+        // Sum from all possible ranks (Formula: "sum of first n natural numbers" (Gauss))
         int rankSum = (populationTourList.size()*(populationTourList.size()+1))/2;
 
-
-        for (int i = 0; i < 100; i++) { // 10% maximumNumberOfIndividualSelected = 10% FEHLERKONTROLLE mod(2)
+        // Iterates through the tourlist to choose the winners (iterationcount = maximumNumberOfIndividualsSelected)
+        for (int i = 0; i < maximumNumberOfIndividualsSelected; i++) {
+            // Random number to choose the winner from the roulette wheel
             double randomInt = Configuration.instance.randomGenerator.nextInt(0, rankSum + 1);
+
+            // Searches the winner in the populationTourList
             for (int j = 0; j < populationTourList.size(); j++) {
+                // randomInt <= (sum of the (previous) ranks with the "sum of first n natural numbers" (Gauss))
                 if (randomInt <= (((j+1)*(j+2))/2)) {
-                    if (returnList.contains(populationTourList.get(j))){
+                    // If the winner is already in the returnList, than this turn of the loop is reset
+                    if (returnPopulationTourList.contains(populationTourList.get(j))){
                         j = populationTourList.size();
                         i--;
                     }
+                    // If the winner is not in the returnList, than he will be added
                     else {
-                        returnList.add(populationTourList.get(j));
+                        returnPopulationTourList.add(populationTourList.get(j));
                     }
                 }
             }
         }
 
-        return returnList;
+        return returnPopulationTourList;
     }
 }
