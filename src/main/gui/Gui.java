c7d37f04c7c3f3354c5a45146cb675f7f282b2f8
Supporterino <lars@roth-kl.de>
2019-01-10 15:05:58 +0100

    Working on JavaFX Base

--- src/main/gui/Gui.java
+++ src/main/gui/Gui.java
@@ -3,10 +3,20 @@ package gui;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.math.BigInteger;
 
 public class Gui extends Application {
 
@@ -16,20 +26,67 @@ public class Gui extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent parent = loader.load();
+
+        Label label = new Label("Drag 'n Drop JSON here");
+        // position of label
+        label.setTranslateY(70);
+        label.setTranslateX(450);
+        label.setTextFill(Color.web("#FFFFFF"));
+
+        // Label dropped = new Label("");
+        VBox dragTarget = new VBox();
+        // dragTarget.getChildren().addAll(label,dropped);
+        dragTarget.setOnDragOver(new EventHandler<DragEvent>() {
 
             @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+            public void handle(DragEvent event) {
+                if (event.getGestureSource() != dragTarget
+                        && event.getDragboard().hasFiles()) {
+                    /* allow for both copying and moving, whatever user chooses */
+                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+                }
+                event.consume();
             }
         });
 
+        dragTarget.setOnDragDropped(new EventHandler<DragEvent>() {
+
+            @Override
+            public void handle(DragEvent event) {
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if (db.hasFiles()) {
+                    //dropped.setText(db.getFiles().toString());
+                    // todo: do something with dropped file
+                    // open JSON file
+                    //readJSONfile();
+                    // todo: get JSON structure from other team
+
+
+                    success = true;
+                }
+                /* let the source know whether the string was successfully
+                 * transferred and used */
+                event.setDropCompleted(success);
+
+                event.consume();
+            }
+        });
+
+
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        // load scene
+        root.getChildren().setAll(parent);
+        // add 'drag and drop'
+        root.getChildren().add(dragTarget);
+
+
+        Scene scene = new Scene(root, 820.0, 484.0);
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Drag Test");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
