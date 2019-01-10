a3f0a1f1cbffe8eb6fca89648503e6fe8f8c1feb
Martin <martin.stoecker99@gmail.com>
2019-01-10 08:31:52 +0100

    clean code

--- src/main/crossover/PartiallyMappedCrossover.java
+++ src/main/crossover/PartiallyMappedCrossover.java
@@ -9,14 +9,15 @@ import java.util.Arrays;
 
 // PMX
 public class PartiallyMappedCrossover extends Crossover {
+
+
+
+    @SuppressWarnings("Duplicates")
     public ArrayList<Tour> doCrossover(Tour tour01, Tour tour02) {
         base.City parent1[] = (base.City[]) tour01.getCities().toArray();
         base.City parent2[] = (base.City[]) tour02.getCities().toArray();
         base.City child1[] = new City[280];
         base.City child2[] = new City[280];
-        ArrayList<base.City> ordered1 = new ArrayList<>();
-        ArrayList<base.City> ordered2 = new ArrayList<>();
-
 
         //new instance of MersenneTwister
         random.MersenneTwisterFast random = new MersenneTwisterFast();
@@ -30,63 +31,74 @@ public class PartiallyMappedCrossover extends Crossover {
 
         //child1 begin
         //copy defined swath variables to output child
-        for(int countSwath = beginOfSwath; beginOfSwath <= endOfSwath; countSwath++) {
+        for(int countSwath = beginOfSwath; countSwath <= endOfSwath; countSwath++) {
             child1[countSwath] = parent1[countSwath];
         }
 
-        for(int countSwath = beginOfSwath; beginOfSwath <= endOfSwath; countSwath++){
-            //checks if city equals city in same position as in parent 2 -> break
-            if(parent1[countSwath] == parent2[countSwath]) {
-                return null; }
-            else{
 
-                // looks for the same number in the swatch -> breaks
-                for(int x = 0; x < endOfSwath - countSwath; x++ )
+            for (int countSwath = beginOfSwath; countSwath <= endOfSwath; countSwath++) {
+                //checks if city equals city in same position as in parent 2 -> break
+                if (parent1[countSwath] == parent2[countSwath]) {
+                    continue;
+                } else {
 
-                    if(parent1[countSwath] == parent2[beginOfSwath+x]){
-                        return null;
-                    }
-                    //takes the number to the position of parent2
-                    else{
-                        for(int y = 0; y <= 277; y++){
-                            if(parent2[y] == parent1[countSwath]){
-                                child1[y]= parent2[y];
+                    // looks for the same number in the swatch -> breaks
+                    for (int x = 0; x < endOfSwath - countSwath; x++)
+
+                        if (parent1[countSwath] == parent2[beginOfSwath + x]) {
+                            continue;
+                        }
+                        //takes the number to the position of parent2
+                        else {
+                            for (int y = 0; y <= 277; y++) {
+                                if (parent2[y] == parent1[countSwath]) {
+                                    child1[y] = parent2[y];
+                                }
                             }
                         }
-                    }
+                }
             }
-        }
-        for(int n = 0; n <=277; n++ ){
-             if(child1[n] == null){
-                 child1[n] = parent2[n];
-             }
-        }
+            for (int n = 0; n <= 277; n++) {
+                if (child1[n] == null) {
+                    child1[n] = parent2[n];
+                }
+            }
+
         //child1 finished
 
         //child2 begin
 
-        for(int countSwath = beginOfSwath; beginOfSwath <= endOfSwath; countSwath++){
-            //checks if city equals city in same position as in parent 2 -> break
+        for(int countSwath = beginOfSwath; countSwath <= endOfSwath; countSwath++){
+
+
+            //checks if city equals city in same position as in parent 1 -> break
+
             if(parent2[countSwath] == parent1[countSwath]) {
-                return null; }
-            else{
+                continue; }
+
+                else{
 
                 // looks for the same number in the swatch -> breaks
+
                 for(int x = 0; x < endOfSwath - countSwath; x++ )
 
                     if(parent2[countSwath] == parent1[beginOfSwath+x]){
-                        return null;
+                        continue;
                     }
-                    //takes the number to the position of parent2
+
+                    //if city is not in swatch parent1 is getting scanned and var goes at that position in output
                     else{
                         for(int y = 0; y <= 277; y++){
                             if(parent1[y] == parent2[countSwath]){
                                 child2[y]= parent1[y];
+                                break;
                             }
                         }
                     }
             }
         }
+
+        //restliche Citys von parent1 in child 2 einfügen
         for(int n = 0; n <=277; n++ ){
             if(child2[n] == null){
                 child2[n] = parent1[n];
