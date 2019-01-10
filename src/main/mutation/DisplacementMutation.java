a2f1f9c4f22807e775d4a1396e87823fce7bcfcd
Rico Kraft <rico3004@gmx.de>
2019-01-10 15:51:10 +0100

    Displacement Mutation implemented

--- src/main/mutation/DisplacementMutation.java
+++ src/main/mutation/DisplacementMutation.java
@@ -1,9 +1,37 @@
 package mutation;
 
+import base.City;
 import base.Tour;
+import configuration.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 // DM
 public class DisplacementMutation extends Mutation {
+
     public void doMutation(Tour tour) {
+        List<City> subTour = new ArrayList<>();
+        int oldIndex = generateRandomNumber(tour.getSize() - 1);
+        int length = generateRandomNumber(tour.getSize() - 1 - oldIndex);
+        int newIndex = generateRandomNumber(tour.getSize() - 1);
+        //create SubTour
+        for (int index = oldIndex + length; index > oldIndex; index--) {
+            subTour.add(tour.getCity(index));
+            tour.getCities().remove(index);
+        }
+        //insert SubTour at newIndex
+        for (int iterations = 0; iterations < length; iterations++) {
+            tour.addCity(newIndex + iterations, subTour.get(iterations));
+        }
+    }
+
+    private int generateRandomNumber(int bound) {
+        int randomValue;
+        do {
+            randomValue = Configuration.instance.randomGenerator.nextInt();
+        } while (randomValue <= bound);
+        return randomValue;
     }
 }
