f1ee26d3616f0fa592d15d61c935d1d0257ce039
Rico Kraft <rico3004@gmx.de>
2019-01-12 14:59:07 +0100

    Exchange Mutation implemented

--- src/main/mutation/ExchangeMutation.java
+++ src/main/mutation/ExchangeMutation.java
@@ -1,8 +1,15 @@
 package mutation;
 
+import base.City;
 import base.Tour;
+import configuration.Configuration;
 
 public class ExchangeMutation extends Mutation {
     public void doMutation(Tour tour) {
+        int firstIndex = Configuration.instance.randomGenerator.nextInt(tour.getSize());
+        int secondIndex = Configuration.instance.randomGenerator.nextInt(tour.getSize());
+        City temCity = tour.getCity(firstIndex);
+        tour.getCities().set(firstIndex, tour.getCity(secondIndex));
+        tour.getCities().set(secondIndex, temCity);
     }
 }
