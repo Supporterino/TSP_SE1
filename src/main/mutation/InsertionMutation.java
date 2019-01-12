9e7e9de377461a6c07c4ae6e56cc04bd9348b376
Rico Kraft <rico3004@gmx.de>
2019-01-12 15:26:03 +0100

    Insertion Mutation implemented

--- src/main/mutation/InsertionMutation.java
+++ src/main/mutation/InsertionMutation.java
@@ -1,8 +1,15 @@
 package mutation;
 
+import base.City;
 import base.Tour;
+import configuration.Configuration;
 
 public class InsertionMutation extends Mutation {
     public void doMutation(Tour tour) {
+        int indexForPicking = Configuration.instance.randomGenerator.nextInt(tour.getSize());
+        City tempCity = tour.getCity(indexForPicking);
+        tour.getCities().remove(indexForPicking);
+        int indexForInsertion = Configuration.instance.randomGenerator.nextInt(tour.getSize());
+        tour.getCities().add(indexForInsertion + 1, tempCity);
     }
 }
