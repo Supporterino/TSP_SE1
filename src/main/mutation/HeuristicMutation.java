dc1b0e10e59e7e86d0642ffdd8154e2178f2132f
Rico Kraft <rico3004@gmx.de>
2019-02-25 11:09:26 +0100

    Validation in Heuristic Mutation added

--- src/main/mutation/HeuristicMutation.java
+++ src/main/mutation/HeuristicMutation.java
@@ -1,9 +1,15 @@
 package mutation;
 
+import base.City;
 import base.Tour;
+import configuration.Configuration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HeuristicMutation extends Mutation {
     public void doMutation(Tour tour) {
-
+        if (tour != null && tour.getCities() != null && tour.getCities().size() > 2) {
+            //do some BS
+        }
     }
 }
