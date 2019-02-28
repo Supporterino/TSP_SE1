88c5c6c4df5bd6781c8a4409000f4df50b1dff9a
estellegraef <estellegraef@users.noreply.github.com>
2019-02-28 21:14:40 +0100

    RouletteWheelSelection finished

--- src/main/selection/ProportionalRouletteWheelSelection.java
+++ src/main/selection/ProportionalRouletteWheelSelection.java
@@ -1,12 +1,13 @@
-package selection;
+package main.selection;
 
-import base.Population;
-import base.Tour;
+import main.base.Population;
+import main.base.Tour;
 
 import java.util.ArrayList;
 
 // PRWS
 public class ProportionalRouletteWheelSelection extends Selection {
+
     public ArrayList<Tour> doSelection(Population population) {
         return null;
     }
--- src/main/selection/RouletteWheelSelection.java
+++ src/main/selection/RouletteWheelSelection.java
@@ -1,13 +1,47 @@
-package selection;
+package main.selection;
 
-import base.Population;
-import base.Tour;
-
-import java.util.ArrayList;
+import main.base.Population;
+import main.base.Tour;
+import java.util.*;
 
 // RWS
 public class RouletteWheelSelection extends Selection {
+
     public ArrayList<Tour> doSelection(Population population) {
-        return null;
+        //calculate total fitness of population
+        double totalFitness = 0;
+        for(Tour tour : population.getTourList()){
+            totalFitness = totalFitness + tour.getFitness();
+        }
+
+        //sort tours by relative fitness
+        SortedMap<Double, Tour> totalFitnessRelativity = new TreeMap<>();
+        for(Tour tour: population.getTourList()){
+            double key = tour.getFitness()/totalFitness;
+            totalFitnessRelativity.put(key, tour);
+        }
+
+        //put tours on a number bar
+        Map<Double, Tour> numberBar = new HashMap<>();
+        double previousProbability = 0;
+        for(Map.Entry<Double, Tour> entry : totalFitnessRelativity.entrySet()){
+            double currentProbability = previousProbability + entry.getKey();
+            numberBar.put(currentProbability, entry.getValue());
+            previousProbability = currentProbability;
+        }
+
+        //choose a tour randomly according to how near it is to
+        ArrayList<Tour> rouletteWheelOrder = new ArrayList<>();
+        while(!numberBar.isEmpty()) {
+            double randomNumber = new random.MersenneTwisterFast().nextDouble();
+            for(Map.Entry<Double, Tour> numberBarSection : numberBar.entrySet()){
+                if(randomNumber <= numberBarSection.getKey()){
+                rouletteWheelOrder.add(numberBarSection.getValue());
+                numberBar.remove(numberBarSection.getKey());
+                break;
+                }
+            }
+        }
+        return rouletteWheelOrder;
     }
 }
