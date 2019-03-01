09c351c4b44bf97de1dd928c91458e2e6e928ff3
deyandanov <37149777+deyandanov@users.noreply.github.com>
2019-03-01 13:44:49 +0100

    added tests

--- src/test/selection/TestRankBasedRouletteWheelSelection.java
+++ src/test/selection/TestRankBasedRouletteWheelSelection.java
@@ -1,4 +1,41 @@
-package selection;
+package test.selection;
+
+import org.junit.Test;
+import main.base.Population;
+import main.base.Tour;
+import main.selection.RankBasedRouletteWheelSelection;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+
+import static org.junit.Assert.assertTrue;
 
 public class TestRankBasedRouletteWheelSelection {
+
+    @Test
+    public void doSelection(){
+
+        Population population = new Population();
+        ArrayList<Tour> tours = new ArrayList<>();
+        Tour child01 = new Tour();
+        Tour child02 = new Tour();
+        tours.add(child01);
+        tours.add(child02);
+        population.setTourList(tours);
+
+        RankBasedRouletteWheelSelection selection = new RankBasedRouletteWheelSelection();
+
+
+        tours =  selection.doSelection(population);
+        assertTrue( selection.doSelection(population) != null);
+
+        LinkedHashSet hashSet = new LinkedHashSet();
+
+        hashSet.addAll(tours);
+
+        assertTrue(hashSet.size()==tours.size());
+
+
+
+    }
 }
--- src/test/selection/TestTournamentSelection.java
+++ src/test/selection/TestTournamentSelection.java
@@ -1,4 +1,38 @@
 package selection;
 
+import main.base.Population;
+import main.base.Tour;
+import main.selection.RouletteWheelSelection;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+
+import static org.junit.Assert.assertTrue;
+
 public class TestTournamentSelection {
+
+    @Test
+    public void doSelection() {
+
+        Population population = new Population();
+        ArrayList<Tour> tours = new ArrayList<>();
+        Tour child01 = new Tour();
+        Tour child02 = new Tour();
+        tours.add(child01);
+        tours.add(child02);
+        population.setTourList(tours);
+
+        RouletteWheelSelection selection = new RouletteWheelSelection();
+
+
+        tours = selection.doSelection(population);
+        assertTrue(selection.doSelection(population) != null);
+
+        LinkedHashSet hashSet = new LinkedHashSet();
+
+        hashSet.addAll(tours);
+
+        assertTrue(hashSet.size() == tours.size());
+    }
 }
