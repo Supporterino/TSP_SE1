cdddacd1e840e87c95cd2183960627d64db28b0e
Jonas <j.hasenauer98@gmail.com>
2019-03-03 18:27:31 +0100

    Init HSQLManager and retrieve data. no usage

--- src/main/reporting/DatabaseInterface/DbConnector.java
+++ src/main/reporting/DatabaseInterface/DbConnector.java
@@ -3,6 +3,7 @@ package reporting.DatabaseInterface;
 import data.HSQLManager;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class DbConnector implements IDbConnector {
 
@@ -10,11 +11,23 @@ public class DbConnector implements IDbConnector {
 
     public DbConnector(String dbName) {
         System.err.println("\nDatabase connection not implemented due to missing documentation of db team\n");
-        //manager=new HSQLManager(dbName);
+        manager = HSQLManager.tsp;
+
+        //Option for other repo
+        //manager = HSQLManager.Knapsack;
     }
 
     @Override
     public ArrayList<Object> getDbDataset() {
+        List<List<Object>> data = manager.getAllScenarios();
+        for (List<Object> subList : data
+        ) {
+            for (Object element : subList
+            ) {
+                //TODO do something the the single elements to build datasets
+            }
+        }
+
         //TODO use manager to retrieve data and repackage in a way that is useful for chart datasets
         return null;
     }
